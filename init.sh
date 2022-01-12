5ae95aebc15c04f1da845164f30ffd0cf85f8fd3
DingSun <xyz031702@hotmail.com>
2022-01-12 09:05:50 +0800

    add files

---
+++ init.sh
@@ -0,0 +1,8 @@
+echo "# log4j-app" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:xyz031702/log4j-app.git
+git push -u origin main
+
